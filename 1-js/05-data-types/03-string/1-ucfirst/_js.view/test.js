0cda0b1b4cf8eb039734c483440287e977cca32d
Alex Kovalenko <lesha.kowalenko@gmail.com>
2021-12-15 10:39:43 +0200

    translation refactoring

--- 1-js/05-data-types/03-string/1-ucfirst/_js.view/test.js
+++ 1-js/05-data-types/03-string/1-ucfirst/_js.view/test.js
@@ -1,9 +1,9 @@
 describe("ucFirst", function() {
-  it('Перший символ у верхньому регістрі', function() {
+  it('Перетворює перший символ у верхній регістр', function() {
     assert.strictEqual(ucFirst("john"), "John");
   });
 
-  it("Не вмирає на порожніх рядках", function() {
+  it('Не вмирає на порожніх рядках', function() {
     assert.strictEqual(ucFirst(""), "");
   });
 });
--- 1-js/05-data-types/03-string/2-check-spam/_js.view/test.js
+++ 1-js/05-data-types/03-string/2-check-spam/_js.view/test.js
@@ -1,13 +1,13 @@
 describe("checkSpam", function() {
-  it('знайдено спам у "buy ViAgRA now"', function() {
+  it('знаходить спам у "buy ViAgRA now"', function() {
     assert.isTrue(checkSpam('buy ViAgRA now'));
   });
 
-  it('знайдено спам у "free xxxxx"', function() {
+  it('знаходить спам у "free xxxxx"', function() {
     assert.isTrue(checkSpam('free xxxxx'));
   });
 
-  it('не знайдено спам у "innocent rabbit"', function() {
+  it('не знаходить спам у "innocent rabbit"', function() {
     assert.isFalse(checkSpam('innocent rabbit'));
   });
 });
